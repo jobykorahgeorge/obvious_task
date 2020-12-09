3a02ffb40c203034eff8e9d35be9d7c00b464a0c
jobykorahwizroots <joby@wizroots.com>
2020-12-09 20:12:21 +0530

    models created

--- app/src/main/java/com/jkg/nasapics/models/ImageDetailsListModel.java
+++ app/src/main/java/com/jkg/nasapics/models/ImageDetailsListModel.java
@@ -1,4 +1,15 @@
 package com.jkg.nasapics.models;
 
+import java.util.List;
+
 public class ImageDetailsListModel {
+    private List<ImageDetailsModel> detailsModelList;
+
+    public List<ImageDetailsModel> getDetailsModelList() {
+        return detailsModelList;
+    }
+
+    public void setDetailsModelList(List<ImageDetailsModel> detailsModelList) {
+        this.detailsModelList = detailsModelList;
+    }
 }
--- app/src/main/java/com/jkg/nasapics/models/ImageDetailsModel.java
+++ app/src/main/java/com/jkg/nasapics/models/ImageDetailsModel.java
@@ -1,4 +1,58 @@
 package com.jkg.nasapics.models;
 
 public class ImageDetailsModel {
+    private String copyright;
+    private String date;
+    private String explanation;
+    private String hdurl;
+    private String title;
+    private String url;
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public void setHdurl(String hdurl) {
+        this.hdurl = hdurl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
